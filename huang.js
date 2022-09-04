c75b2fa805a847a0958b3d49da90978db72b9558
huangwj2 <53824987+huangwj2@users.noreply.github.com>
2022-09-04 21:07:14 +0200

    Revert "Add copy function to address and email"
    This reverts commit fad7e5821e56d10ab4bbb7864eb0e092fe4dec86.

--- huang.js
+++
@@ -1,7 +0,0 @@
-function copy(text) {
-  // var text = document.getElementById("idOfText");
-  // text.select();
-  // text.setSelectionRange(0, 99999);
-  // navigator.clipboard.writeText(text.value);
-  navigator.clipboard.writeText(text);
-}
