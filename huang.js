fad7e5821e56d10ab4bbb7864eb0e092fe4dec86
huangwj2 <53824987+huangwj2@users.noreply.github.com>
2022-09-04 16:30:36 +0200

    Add copy function to address and email

---
+++ huang.js
@@ -0,0 +1,7 @@
+function copy(text) {
+  // var text = document.getElementById("idOfText");
+  // text.select();
+  // text.setSelectionRange(0, 99999);
+  // navigator.clipboard.writeText(text.value);
+  navigator.clipboard.writeText(text);
+}
